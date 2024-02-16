096a193132fdbb36055324b46123c9d6dfdcd24d
nliyanage <nethmin.liyanage@gmail.com>
2024-02-16 09:49:44 -0800

    problem 2

--- sql/02.sql
+++ sql/02.sql
@@ -2,3 +2,7 @@
  * Find the actor_id of every actor whose first name starts with 'j'.
  * Order the results from low to hi.
  */
+
+select actor_id from actor where first_name ilike 'j%' order by actor_id;
+
+--order "nondeterministic"
