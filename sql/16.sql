44b55c4d1e0c6a38f4040c4572aae4fb4675aad9
nliyanage <nethmin.liyanage@gmail.com>
2024-02-22 21:12:34 -0800

    tests passing

--- sql/16.sql
+++ sql/16.sql
@@ -3,3 +3,4 @@
  * where the profit is the total amount that customer have payer for the film.
  * Use tables payment, rental, inventory, and film. 
  */
+select film.title, sum(payment.amount) as profit from film join inventory using (film_id) join rental using (inventory_id) join payment using (rental_id) group by film.title order by profit desc; 
