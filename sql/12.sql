b43ece9b82d83099c9bcad963ed70037e99f0332
nliyanage <nethmin.liyanage@gmail.com>
2024-02-22 19:01:47 -0800

    12

--- sql/12.sql
+++ sql/12.sql
@@ -3,3 +3,4 @@
  * List the customers alphabetically by last name.
  * Use tables payment and customer.
  */
+SELECT customer.customer_id, customer.first_name, customer.last_name, sum(payment.amount) AS sum FROM customer JOIN payment USING (customer_id) GROUP BY customer_id, first_name, last_name ORDER BY last_name, first_name;
